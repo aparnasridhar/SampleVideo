53d7cac966997107be64d2f6b4c1a76ddc22c4ca
Aparna Sridhar <apar.sridhar@gmail.com>
2014-12-22 09:50:56 +0530

    Initial Commit

---
+++ SampleVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleVideo
+//
+//  Created by APARNA SRIDHAR on 12/22/14.
+//  Copyright (c) 2014 APARNA SRIDHAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
