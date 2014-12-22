3a6d351990aeb389071b82730b6e45f4ce19b6a1
Aparna Sridhar <apar.sridhar@gmail.com>
2014-12-22 11:02:25 +0530

    SampleVideo App
    How to use the video player and play a simple video in your view.

--- SampleVideo/ViewController.swift
+++ SampleVideo/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
-
+    
+    var moviePlayer : MPMoviePlayerController?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        playVideo()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func playVideo() {
+        
+        //Get the Video Path
+        //You need to put this in Project->Target->copy bundle resource for this to work
+        let videoPath = NSBundle.mainBundle().pathForResource("cat", ofType:"mp4")
+        
+        //Make a URL from your path
+        let url = NSURL.fileURLWithPath(videoPath!)
+        
+        //Initalize the movie player
+        moviePlayer = MPMoviePlayerController(contentURL: url)
+        
+        if let player = moviePlayer {
+            
+            //Make the player scale the entire view
+            player.view.frame = self.view.bounds
+            player.scalingMode = .AspectFill
+            
+            //Add it as a subView to your currentView
+            self.view.addSubview(player.view)
+            
+            //Play the video
+            player.prepareToPlay()
+
+        }
+        else {
+            println("Movie player couldn't be initialized")
+        }
+    }
 
 }
 
